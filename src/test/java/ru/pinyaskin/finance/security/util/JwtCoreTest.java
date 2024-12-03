02f1438349f7fda1e118c172b84d823f8843b3b3
Iliya Pinyaskin <pinyaskiniliya163@gmail.com>
2024-12-03 12:27:04 +0300

    added jacoco, refactoring jwt core

--- src/test/java/ru/pinyaskin/finance/security/util/JwtCoreTest.java
+++ src/test/java/ru/pinyaskin/finance/security/util/JwtCoreTest.java
@@ -1,6 +1,9 @@
 package ru.pinyaskin.finance.security.util;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.pinyaskin.finance.model.entity.User;
+import ru.pinyaskin.finance.security.UserPrincipal;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -8,12 +11,15 @@ public class JwtCoreTest {
     @Test
     void shouldReturnValidKey() {
         JwtCore jwt = new JwtCoreImpl("secret-key", 600000);
-        String token = jwt.generateToken("user");
+        User user = new User();
+        user.setEmail("john@mail.ru");
+
+        String token = jwt.generateToken(new UserPrincipal(user));
         boolean isExpired = jwt.isTokenExpired(token);
         String username = jwt.getUsernameFromToken(token);
 
         assertThat(token).isNotNull();
         assertThat(isExpired).isFalse();
-        assertThat(username).isEqualTo("user");
+        assertThat(username).isEqualTo("john@mail.ru");
     }
 }
